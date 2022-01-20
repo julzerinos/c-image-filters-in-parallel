f309314995128978ac1412b3c3f88d616734a60b
kszmmnn <kubiszewskiszymon@gmail.com>
2022-01-20 20:31:34 +0100

    poprawienie bledu

--- filters/convolution.c
+++ filters/convolution.c
@@ -12,9 +12,9 @@ void create_blur_kernel(int dimension, double kernel[dimension][dimension])
     double mean = W / 2;
     double sum = 0.0;
     for (int x = 0; x < W; ++x)
-        for (int y = 0; y < W; ++y)
-        {
-            kernel[x][y] = exp(-0.5 * (pow((x - mean) / sigma, 2.0) + pow((y - mean) / sigma, 2.0))) / (2 * M_PI * sigma * sigma);
+        for (int y = 0; y < W; ++y) {
+            kernel[x][y] = exp(-0.5 * (pow((x - mean) / sigma, 2.0) + pow((y - mean) / sigma, 2.0))) /
+                           (2 * M_PI * sigma * sigma);
 
             // Accumulate the kernel values
             sum += kernel[x][y];
@@ -26,22 +26,21 @@ void create_blur_kernel(int dimension, double kernel[dimension][dimension])
             kernel[x][y] /= sum;
 }
 
-void create_edge_detection_kernel(int dimension, double kernel[dimension][dimension])
-{
- kernel[0][0] = 0;
- kernel[0][1] = 1;
- kernel[0][2] = 0;
- kernel[1][0] = 1;
- kernel[1][1] = -4;
- kernel[1][2] = 1;
- kernel[2][0] = 0;
- kernel[2][1] = 1;
- kernel[2][2] = 0;
+void create_edge_detection_kernel(int dimension, double kernel[dimension][dimension]) {
+    kernel[0][0] = 0;
+    kernel[0][1] = 1;
+    kernel[0][2] = 0;
+    kernel[1][0] = 1;
+    kernel[1][1] = -4;
+    kernel[1][2] = 1;
+    kernel[2][0] = 0;
+    kernel[2][1] = 1;
+    kernel[2][2] = 0;
 }
 
 
-void apply_convolution_sequentially(int height, int width, RGBTRIPLE image[height][width], int kernel_dimension, double kernel[kernel_dimension][kernel_dimension])
-{
+void apply_convolution_sequentially(int height, int width, RGBTRIPLE image[height][width], int kernel_dimension,
+                                    double kernel[kernel_dimension][kernel_dimension]) {
     RGBTRIPLE temp_img[height][width];
 
     for (int i = 0; i < height; i++)
@@ -50,8 +49,7 @@ void apply_convolution_sequentially(int height, int width, RGBTRIPLE image[heigh
 
     for (int i = 0; i < height; i++) // copying contents of new image to the original image
     {
-        for (int j = 0; j < width; j++)
-        {
+        for (int j = 0; j < width; j++) {
             image[i][j].rgbtBlue = temp_img[i][j].rgbtBlue;
             image[i][j].rgbtGreen = temp_img[i][j].rgbtGreen;
             image[i][j].rgbtRed = temp_img[i][j].rgbtRed;
@@ -59,16 +57,16 @@ void apply_convolution_sequentially(int height, int width, RGBTRIPLE image[heigh
     }
 }
 
-void apply_convolution(int x, int y, int height, int width, RGBTRIPLE image[height][width], RGBTRIPLE target_image[height][width], int kernel_dimension, double kernel[kernel_dimension][kernel_dimension])
-{
+void apply_convolution(int x, int y, int height, int width, RGBTRIPLE image[height][width],
+                       RGBTRIPLE target_image[height][width], int kernel_dimension,
+                       double kernel[kernel_dimension][kernel_dimension]) {
     float totalRed = 0.0;
     float totalGreen = 0.0;
     float totalBlue = 0.0;
 
     int kernel_spread = kernel_dimension / 2;
     for (int i = -kernel_spread; i <= kernel_spread; i++)
-        for (int j = -kernel_spread; j <= kernel_spread; j++)
-        {
+        for (int j = -kernel_spread; j <= kernel_spread; j++) {
             int xIndex = x + i;
             int yIndex = y + j;
 
@@ -85,8 +83,8 @@ void apply_convolution(int x, int y, int height, int width, RGBTRIPLE image[heig
     target_image[x][y].rgbtGreen = round(totalGreen);
 }
 
-void apply_convolution_parallelly(int thread_count, int height, int width, RGBTRIPLE image[height][width], int kernel_dimension, double kernel[kernel_dimension][kernel_dimension])
-{
+void apply_convolution_parallelly(int thread_count, int height, int width, RGBTRIPLE image[height][width],
+                                  int kernel_dimension, double kernel[kernel_dimension][kernel_dimension]) {
     RGBTRIPLE temp_img[height][width];
 
     omp_set_dynamic(0);
@@ -98,20 +96,21 @@ void apply_convolution_parallelly(int thread_count, int height, int width, RGBTR
             for (int j = 0; j < width; j++)
                 apply_convolution(i, j, height, width, image, temp_img, kernel_dimension, kernel);
 
-        for (int i = 0; i < height; i++) // copying contents of new image to the original image
-        {
-            for (int j = 0; j < width; j++) {
-                image[i][j].rgbtBlue = temp_img[i][j].rgbtBlue;
-                image[i][j].rgbtGreen = temp_img[i][j].rgbtGreen;
-                image[i][j].rgbtRed = temp_img[i][j].rgbtRed;
-            }
+    }
+    for (int i = 0; i < height; i++) // copying contents of new image to the original image
+    {
+        for (int j = 0; j < width; j++) {
+            image[i][j].rgbtBlue = temp_img[i][j].rgbtBlue;
+            image[i][j].rgbtGreen = temp_img[i][j].rgbtGreen;
+            image[i][j].rgbtRed = temp_img[i][j].rgbtRed;
         }
     }
 }
 
 
-void edge_detection_parallelly(int thread_count, int height, int width, RGBTRIPLE image[height][width], int kernel_dimension, double kernel[kernel_dimension][kernel_dimension])
-{
+void
+edge_detection_parallelly(int thread_count, int height, int width, RGBTRIPLE image[height][width], int kernel_dimension,
+                          double kernel[kernel_dimension][kernel_dimension]) {
     create_blur_kernel(kernel_dimension, kernel);
     apply_convolution_parallelly(thread_count, height, width, image, kernel_dimension, kernel);
     create_edge_detection_kernel(kernel_dimension, kernel);
@@ -119,22 +118,22 @@ void edge_detection_parallelly(int thread_count, int height, int width, RGBTRIPL
 }
 
 
-void edge_detection_sequentially(int height, int width, RGBTRIPLE image[height][width], int kernel_dimension, double kernel[kernel_dimension][kernel_dimension])
-{
+void edge_detection_sequentially(int height, int width, RGBTRIPLE image[height][width], int kernel_dimension,
+                                 double kernel[kernel_dimension][kernel_dimension]) {
     create_blur_kernel(kernel_dimension, kernel);
     apply_convolution_sequentially(height, width, image, kernel_dimension, kernel);
     create_edge_detection_kernel(kernel_dimension, kernel);
     apply_convolution_sequentially(height, width, image, kernel_dimension, kernel);
 }
 
-void blur_sequentially(int height, int width, RGBTRIPLE image[height][width], int kernel_dimension, double kernel[kernel_dimension][kernel_dimension])
-{
+void blur_sequentially(int height, int width, RGBTRIPLE image[height][width], int kernel_dimension,
+                       double kernel[kernel_dimension][kernel_dimension]) {
     create_blur_kernel(kernel_dimension, kernel);
     apply_convolution_sequentially(height, width, image, kernel_dimension, kernel);
 }
 
-void blur_parallelly(int thread_count, int height, int width, RGBTRIPLE image[height][width], int kernel_dimension, double kernel[kernel_dimension][kernel_dimension])
-{
+void blur_parallelly(int thread_count, int height, int width, RGBTRIPLE image[height][width], int kernel_dimension,
+                     double kernel[kernel_dimension][kernel_dimension]) {
     create_blur_kernel(kernel_dimension, kernel);
     apply_convolution_parallelly(thread_count, height, width, image, kernel_dimension, kernel);
 }
